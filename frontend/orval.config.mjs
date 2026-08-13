9a3a99eb5375085f12a1d00e7c4305ad5cd87f85
yuwen.ding <dingyuwen777@gmail.com>
2026-08-13 15:35:13 +0800

    修正 Orval Fetch 返回类型配置

--- frontend/orval.config.mjs
+++ frontend/orval.config.mjs
@@ -12,7 +12,6 @@ export default defineConfig({
       override: {
         fetch: {
           includeHttpResponseReturnType: false,
-          forceSuccessResponse: true,
         },
       },
     },
