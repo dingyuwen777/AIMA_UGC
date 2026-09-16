82affeb6cf1e7842adda34d5afc0b847b2326116
yuwen.ding <dingyuwen777@gmail.com>
2026-09-17 07:24:34 +0800

    恢复管理员配置稳定页面入口

---
+++ frontend/src/features/admin-configuration/pages/AdminConfigurationPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import AdminConfigurationPage from './AdminConfigurationPage/AdminConfigurationPage.vue'
+</script>
+
+<template>
+  <AdminConfigurationPage />
+</template>
