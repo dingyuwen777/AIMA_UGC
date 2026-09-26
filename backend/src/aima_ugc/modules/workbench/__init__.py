9c4b724c60b16e1e7baabecc86a768eb22f6d413
yuwen.ding <dingyuwen777@gmail.com>
2026-09-27 01:46:04 +0800

    增加工作台 Contract 与模块边界

---
+++ backend/src/aima_ugc/modules/workbench/__init__.py
@@ -0,0 +1,5 @@
+"""工作台模块。"""
+
+from .http import WorkbenchAnalysisUnavailable, WorkbenchHttpService, WorkbenchLayoutConflict
+
+__all__ = ["WorkbenchAnalysisUnavailable", "WorkbenchHttpService", "WorkbenchLayoutConflict"]
